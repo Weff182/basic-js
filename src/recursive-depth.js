3a8ecb7d0fb2dfb0f78eec000cd6aa302da4aaa1
Weff182 <weffomax@gmail.com>
2021-03-18 20:31:19 +0300

    feat: added new functionality

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -2,20 +2,19 @@ const CustomError = require("../extensions/custom-error");
 
 module.exports = class DepthCalculator {
   calculateDepth(b) {
-    let m = 0;
+    let m = 1;
     for(let i = 0; i <= b.length; i++) {
       if (typeof(b[i]) === 'object') {
-        m = m + 2;
+        m = m + 1;
         b = b[i]
         rec(b)
       }
     }
     return m; 
-    }
-    
+    } 
 };
 
-function rec() {
+function rec(b) {
   for(let i = 0; i <= b.length; i++) {
   if (typeof(b[i]) === 'object') {
     m = m + 1;
