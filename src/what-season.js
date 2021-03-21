c61937d5114d151203241668e910c15913b0f65e
Weff182 <weffomax@gmail.com>
2021-03-21 23:33:14 +0300

    feat: added new functionality

--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,10 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function getSeason(date) {
+  if (date === undefined) {
+    return 'Unable to determine the time of year!';
+  }
+ 
   if (date.getMonth() === 11 || date.getMonth() === 0 || date.getMonth() === 1) {
     return 'winter'
   }
@@ -13,6 +17,7 @@ module.exports = function getSeason(date) {
   if (date.getMonth() === 8 || date.getMonth() === 9 || date.getMonth() === 10) {
     return 'autumn (fall)'
   }
+
   else {
     return 'Error'
   }
